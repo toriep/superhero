664900c916ee9365f488ff20243b578151bb8b24
Hongyi Zhang <hongyizhang@Hongyis-MacBook-Pro.local>
2019-03-18 20:01:33 -0700

    adding creating new test

--- test/users.spec.js
+++ test/users.spec.js
@@ -2,17 +2,19 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../src');
 
+
 chai.should();
 
 chai.use(chaiHttp);
 
-describe('Server should be healthy', () => {
-  it('it should GET health', done => {
-    chai.request(server).get('/health').end((err, res) => {
+describe('Server should be able to perform "/users" operation', () => {
+  it('it should get /users/foods', done => {
+    chai.request(server).get('/users/foods').end((err, res) => {
       res.should.have.status(200);
       res.should.be.a('object');
-      res.body.should.have.property('status').eql('UP');
+      res.body.should.be.a('array');
+ 
       done();
     })
-  })
+  });
 })
