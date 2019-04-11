b79b3ebd0ab44a113c601cad9ac16c586ee82336
Torie Pham <tientp@uci.edu>
2019-04-10 23:06:16 -0700

    set heroes display to 2 columns on large screens

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './bootstrap/css/bootstrap.min.css';
 import Card from './Card';
-import AddHero from  './AddHero';
 
 class App extends Component {
   state = {
@@ -24,7 +23,6 @@ class App extends Component {
   handleAddItem = async (event) => {
     event.preventDefault();
     const { hero_name, first_name, last_name, favorite_food, superHeroes } = this.state;
-    const newHero = { hero_name, first_name, last_name };
     await axios.post('http://localhost:3001/users/food', { hero_name, first_name, last_name, favorite_food }).then((resp)=>{
       this.setState({
         superHeroes: resp.data.usersCopy
@@ -37,12 +35,9 @@ class App extends Component {
     const { hero_name, first_name, last_name, favorite_food } = this.state;
     const heroesList = superHeroes.map(index => {
       const { favorite_food, first_name, hero_name, last_name } = index;
-      //only display entries that have truthy values for all of these fields
-      // if(favorite_food && first_name && hero_name && last_name){
         return (
         <Card key={ index.id } hero={ index } />
         )
-      // };
     });
 
     return(
@@ -87,8 +82,9 @@ class App extends Component {
             </div>
           </div>
         </form>
-        {/* <AddHero /> */}
-        { heroesList }
+        <div className="row m-3">
+          { heroesList }
+        </div>
       </div>
     )
   }
