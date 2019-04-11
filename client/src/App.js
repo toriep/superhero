c04089f568e6aed97b825adfe21251b6d217982d
Torie Pham <tientp@uci.edu>
2019-04-10 22:51:43 -0700

    adjusted endpoint for adding heroes

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,92 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './bootstrap/css/bootstrap.min.css';
 import Card from './Card';
+import AddHero from  './AddHero';
 
 class App extends Component {
   state = {
-    superHeroes: []
+    superHeroes: [],
+    hero_name: '',
+    first_name: '',
+    last_name: '',
+    favorite_food: ''
   }
 
   componentDidMount() {
-    const url = 'http://localhost:3001/users/foods';
-    axios.get(url).then((resp)=>{
-      console.log('resp :', resp);
+    axios.get('http://localhost:3001/users/foods').then((resp)=>{
       this.setState({
         superHeroes: resp.data
       })
     });
   }
 
+  handleAddItem = async (event) => {
+    event.preventDefault();
+    const { hero_name, first_name, last_name, favorite_food, superHeroes } = this.state;
+    const newHero = { hero_name, first_name, last_name };
+    await axios.post('http://localhost:3001/users/food', { hero_name, first_name, last_name, favorite_food }).then((resp)=>{
+      this.setState({
+        superHeroes: resp.data.usersCopy
+      })
+    });
+  }
+
   render(){
     const { superHeroes } = this.state;
-    const heroesList = superHeroes.map( index => {
+    const { hero_name, first_name, last_name, favorite_food } = this.state;
+    const heroesList = superHeroes.map(index => {
       const { favorite_food, first_name, hero_name, last_name } = index;
       //only display entries that have truthy values for all of these fields
-      if(favorite_food && first_name && hero_name && last_name){
+      // if(favorite_food && first_name && hero_name && last_name){
         return (
         <Card key={ index.id } hero={ index } />
         )
-      }
+      // };
     });
+
     return(
       <div className="container text-center mt-4 p-3">
         <h1>Super Heroes</h1>
+        <form onSubmit={this.handleAddItem}>
+          <div className="row">
+            <div className="col s6 offset-s3">
+              <label>Hero</label>
+              <input onChange={(e)=>this.setState({hero_name: e.target.value})} 
+              type="text" 
+              value={hero_name}/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s6 offset-s3">
+              <label>First Name</label>
+              <input onChange={({target})=>this.setState({first_name:target.value})}//you can destructure stuff in your paramter
+              type="text" 
+              value={first_name}/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s6 offset-s3">
+              <label>Last Name</label>
+              <input onChange={({target})=>this.setState({last_name:target.value})}//you can destructure stuff in your paramter
+              type="text" 
+              value={last_name}/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s6 offset-s3">
+              <label>Favorite Food</label>
+              <input onChange={({target})=>this.setState({favorite_food:target.value})}//you can destructure stuff in your paramter
+              type="text" 
+              value={favorite_food}/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s6 offset-s3 right-align">
+              <button className="btn teal lighten-2">Add Hero</button>
+            </div>
+          </div>
+        </form>
+        {/* <AddHero /> */}
         { heroesList }
       </div>
     )
